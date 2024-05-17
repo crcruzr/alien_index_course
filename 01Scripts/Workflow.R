db68faaa3041fc4e297e007a8ea4f278ac728078
crcruzr <cristian.alexander.cruz.rodriguez@umontreal.ca>
2024-05-17 10:48:38 -0400

    new data added

--- 01Scripts/Workflow.R
+++ 01Scripts/Workflow.R
@@ -6,6 +6,7 @@ library(maps)
 library(ggplot2)
 library(terra)
 library(geodata)
+library(tidyverse)
 
 setwd('/Users/ccruzr/Library/Mobile Documents/com~apple~CloudDocs/Cristian/Documents/Estudios/Postgrado/PhD/Courses/Modeling and Indicators Bios2/alien_index_course')
 getwd()
@@ -186,9 +187,177 @@ for(i in 1:length(sppq)) {
 }
 
 #write.csv(spp_area, '02ProcessedData/_invasive_spp.csv', row.names = F)
-spp_area <- read.csv('/Users/ccruzr/Library/Mobile Documents/com~apple~CloudDocs/Cristian/Documents/Estudios/Postgrado/PhD/Courses/Modeling and Indicators Bios2/Alien_Indicator/02ProcessedData/area_invasive_spp.csv', header = T)
+spp_area <- data.table::fread('02ProcessedData/area_invasive_spp.csv', header = T)
 spp_area2 <- (log1p(spp_area[,c(1:54)]))
 spp_area2$species <- spp_area$species
 names(spp_area)
 
+
+##Rasterize 
+#1000 = 1 Km@
+r <- rast(qc_pla, crs= 'EPSG:32198')
+res(r) <- 10000  # in meters
+qc_r <- rasterize(qc_pla, r)
+qc_r <- crop(qc_r, qc_pla)
+plot(qc_r)
+r
+table(is.na(mapp_qc_t_70$year))
+mapp_qc_t_70<- mapp_qc_t_70[!is.na(mapp_qc_t_70$year),]
+table(is.na(mapp_qc_t_70$year))
+
+##
+
+spp_f <- unique(mapp_qc_t_70$species)
+
+mapp_qc_t_70$id <- 1:nrow(mapp_qc_t_70)
+
+mapp_qc_t_70<- mapp_qc_t_70[-51462,] ## record outside of the area
+
+
+win_area_ras <- as.data.frame(matrix(nrow = length(spp_f), ncol = 51))
+names(win_area_ras) <- c(paste0('win_',1970:2020))
+rownames(win_area_ras) <- spp_f
+
+#w<- win_area_ras
+win_area_ras <- w
+
+## to calculate the number of pixels per window
+
+for(i in 1:ncol(win_area_ras)){
+    pb <- txtProgressBar(min = 0, max = 100, style = 3)
+    setTxtProgressBar(pb, (i*100)/ncol(win_area_ras))
+
+window.i <- mapp_qc_t_70[mapp_qc_t_70$year >= (1969+i) & mapp_qc_t_70$year <= (1973+i),] 
+  for (j in 1:length(spp_f)) {
+    window.i.j <- window.i[window.i$species %in% spp_f[j],]
+      if (!nrow(window.i.j) == 0) {
+      sp <- vect(window.i.j, geom = c('x', 'y'), crs= 'EPSG:32198')
+      spp.j <- rasterize(sp, qc_r, "species", function(x, ...) length(unique(na.omit(x))))
+      num_valid_pixels <- sum(freq(spp.j)$count)
+      win_area_ras[spp_f[j],i] <- num_valid_pixels
+      } else {
+       win_area_ras[spp_f[j],i] <- 0 
+      }
+    }
+}
+
+i
+j
+copy <- win_area_ras
+win_area_ras[win_area_ras == 0] <- NA
+
+
+### Graphics
+win_area_ras$species <- rownames(win_area_ras)
+# create data with less columns 
+win_area_ras_long <- pivot_longer(win_area_ras, cols = -species, names_to = "Window", values_to = "Value")
+row.names(win_area_ras)
+
+win_area_ras_long$Valuelog <- (log1p(win_area_ras_long$Value))
+
+## without log
+win_area_ras_long %>%
+  group_by(Window) %>%
+  summarize(median = quantile(Value, p= 0.5, na.rm = TRUE) , 
+            q1 = quantile(Value, p = 0.25, na.rm = TRUE),
+            q3 = quantile(Value, p = 0.75, na.rm = TRUE)) %>%
+ggplot(aes(x = Window, y = median)) +
+  geom_point() +
+  geom_errorbar(aes(ymin = q1, ymax = q3)) +
+    geom_line(aes(x = Window, y = q1))+
+     theme_bw()+
+    theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+win_area_ras_long$Window <- gsub('win_', '', win_area_ras_long$Window)
+
+## with log
+graph1 <-win_area_ras_long %>%
+  group_by(Window) %>%
+  summarize(mean = mean(Valuelog,na.rm = TRUE) , 
+            q1 = quantile(Valuelog, p = 0.25, na.rm = TRUE),
+            q3 = quantile(Valuelog, p = 0.75, na.rm = TRUE)) %>%
+ggplot( aes(x = Window, y = mean)) +
+  geom_errorbar(aes(ymin = q1, ymax = q3), col = '#000099') +
+  geom_point(size = 10, col = '#FF8000') +
+    #geom_line(aes(x = Window, y = q1)) +
+     theme_bw()+
+    theme(axis.text.x = element_text(angle = 60, hjust = 1,size = 24),
+    axis.text.y = element_text(size = 24),
+     axis.title.x = element_text(size = 26),  # Change x-axis title size
+  axis.title.y = element_text(size = 26),  # Change y-axis title size
+    )
+    
+
+png("04Graphics/rec_Xwind_5Y_10km2_log.png", width = 2080, height = 1080)
+print(graph1)
+dev.off()
+
+
+## to calculate the area per poligon
+
+win_area_vec <- as.data.frame(matrix(nrow = length(spp_f), ncol = 51))
+names(win_area_vec) <- c(paste0('win_',1970:2020))
+rownames(win_area_vec) <- spp_f
+
+for(i in 1:ncol(win_area_vec)){
+    pb <- txtProgressBar(min = 0, max = 100, style = 3)
+    setTxtProgressBar(pb, (i*100)/ncol(win_area_vec))
+
+window.i <- mapp_qc_t_70[mapp_qc_t_70$year >= (1969+i) & mapp_qc_t_70$year <= (1973+i),] 
+  for (j in 1:length(spp_f)) {
+    window.i.j <- window.i[window.i$species %in% spp_f[j],]
+      if (nrow(window.i.j) > 2) {
+      i.t.y.p <- vect(window.i.j, geom =c('x', 'y'), crs= 'EPSG:32198' )
+      i.t.y.ch<- convHull(i.t.y.p)
+      num_valid_poligons <- m2_to_ha(expanse(i.t.y.ch))
+      win_area_vec[spp_f[j],i] <- num_valid_poligons
+      } else if (nrow(window.i.j) == 2 | nrow(window.i.j) == 1) {
+      win_area_vec[spp_f[j],i] <- nrow(window.i.j)
+    # species without records
+    } else if (nrow(window.i.j) == 0) {
+      win_area_vec[spp_f[j],i] <- NA
+    }
+  }
+}
+
+### Graphics
+win_area_vec$species <- rownames(win_area_vec)
+# create data with less columns 
+win_area_vec_long <- pivot_longer(win_area_vec, cols = -species, names_to = "Window", values_to = "Value")
+
+win_area_vec_long$Valuelog <- (log1p(win_area_vec_long$Value))
+
+## without log
+win_area_vec_long %>%
+  group_by(Window) %>%
+  summarize(median = quantile(Value, p= 0.5, na.rm = TRUE) , 
+            q1 = quantile(Value, p = 0.25, na.rm = TRUE),
+            q3 = quantile(Value, p = 0.75, na.rm = TRUE)) %>%
+ggplot(aes(x = Window, y = median)) +
+  geom_point() +
+  geom_errorbar(aes(ymin = q1, ymax = q3)) +
+    geom_line(aes(x = Window, y = q1)) 
+
+## with log
+win_area_vec_long %>%
+  group_by(Window) %>%
+  summarize(median = quantile(Valuelog, p= 0.5, na.rm = TRUE) , 
+            q1 = quantile(Valuelog, p = 0.25, na.rm = TRUE),
+            q3 = quantile(Valuelog, p = 0.75, na.rm = TRUE)) %>%
+ggplot(aes(x = Window, y = median)) +
+  geom_point() +
+  geom_errorbar(aes(ymin = q1, ymax = q3)) +
+    geom_line(aes(x = Window, y = q1)) 
+
+
+write.csv(win_area_vec_long, '02ProcessedData/Windo_5Yr_vector.csv')
+write.csv(win_area_ras_long, '02ProcessedData/Windo_5Yr_raster.csv')
+write.csv(win_area_ras, '02ProcessedData/Windo_5Yr_raster_ext.csv')
+
+writeRaster(qc_r, '02ProcessedData/Quebec_planar_raster.tif')
+qc_r
+plot(qc_r)
+
+
+### plots 
 
